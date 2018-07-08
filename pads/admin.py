6a527a33267189aa6aaa5dcfa9c9b411c6375d77
Jon <jon.drew1492@gmail.com>
2018-07-08 19:09:09 -0400

    Added new apps

--- pads/admin.py
+++ pads/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Pad
 
 # Register your models here.
+
+admin.site.register(Pad)
--- pond/settings.py
+++ pond/settings.py
@@ -112,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'US'
+TIME_ZONE = 'America/Toronto'
 
 USE_I18N = True
 
