b7288abcabf2b15e2ba51c7062ced46d59090c7a
Jon <jon.drew1492@gmail.com>
2018-07-09 20:09:59 -0400

    Finished prototype pads

--- events/models.py
+++ events/models.py
@@ -1,4 +1,14 @@
+from __future__ import unicode_literals
+
+import random
+import os
+
 from django.db import models
+from django.db.models import Q
+from django.db.models.signals import pre_save, post_save
+from django.urls import reverse
+
+from .utils import unique_slug_generator
 from django.utils import timezone
 
 from pads.models import Pad
@@ -10,13 +20,15 @@ def default_end():
   return timezone.now() + timezone.timedelta(hours=1) + timezone.timedelta(days=1)
 
 class Event(models.Model):
-    start = models.DateTimeField(default=default_start)
-    end = models.DateTimeField(default=default_end)
-    pad = models.ForeignKey('pads.Pad', on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_at = models.DateTimeField(default=timezone.now)
-    published_at = models.DateTimeField(blank=True, null=True)
+    start           = models.DateTimeField(default=default_start)
+    end             = models.DateTimeField(default=default_end)
+    pad             = models.ForeignKey('pads.Pad', on_delete=models.CASCADE)
+    title           = models.CharField(max_length=200)
+    text            = models.TextField()
+    active          = models.BooleanField(default=True)
+    slug            = models.SlugField(null=True, unique=True, editable=False)
+    created_at      = models.DateTimeField(default=timezone.now)
+    published_at    = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.published_at = timezone.now()
@@ -24,3 +36,60 @@ class Event(models.Model):
 
     def __str__(self):
         return self.title
+
+class EventQuerySet(models.query.QuerySet):
+    def active(self):
+        return self.filter(active=True)
+
+    def search(self, query):
+        lookups = (Q(title__icontains=query) |
+            Q(description__icontains=query) |
+            Q(tag__title__icontains=query))
+        return self.filter(lookups).distinct()
+
+    def get_absolute_url(self):
+        return reverse('pads:detail', kwargs={'slug': self.slug})
+
+    def __str__(self):
+        return self.title
+
+    def __unicode__(self):
+        return self.title
+
+def event_pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+pre_save.connect(event_pre_save_receiver, sender=Event)
+
+def get_filename_ext(filepath):
+    base_name = os.path.basename(filepath)
+    name, ext = os.path.splitext(base_name)
+    return name, ext
+
+class EventQuerySet(models.query.QuerySet):
+    def active(self):
+        return self.filter(active=True)
+
+    def search(self, query):
+        lookups = (Q(title__icontains=query) |
+            Q(description__icontains=query) |
+            Q(price__icontains=query) |
+            Q(tag__title__icontains=query))
+        return self.filter(lookups).distinct()
+
+class EventManager(models.Manager):
+    def get_queryset(self):
+        return EventQuerySet(self.model, using=self._db)
+
+    def all(self):
+        return self.get_queryset().active()
+
+    def get_by_id(self, id):
+        qs = self.get_queryset().filter(id=id)
+        if qs.count() == 1:
+            return qs.first()
+        return None
+
+    def search(self, query):
+        return self.get_queryset().active().search(query)
--- pads/models.py
+++ pads/models.py
@@ -12,10 +12,11 @@ from .utils import unique_slug_generator
 from django.utils import timezone
 
 class Pad(models.Model):
+    name            = models.CharField(default="My Pad", max_length=200)
     address         = models.TextField()
-    owner           = models.CharField(max_length=200)
-    description     = models.TextField()
+    description     = models.TextField(max_length=200)
     active          = models.BooleanField(default=True)
+    slug            = models.SlugField(null=True, unique=True, editable=False)
     created_at      = models.DateTimeField(default=timezone.now)
     published_at    = models.DateTimeField(blank=True, null=True)
 
@@ -24,7 +25,7 @@ class Pad(models.Model):
         self.save()
 
     def __str__(self):
-        return self.title
+        return self.name
 
 class PadQuerySet(models.query.QuerySet):
     def active(self):
@@ -56,16 +57,6 @@ def get_filename_ext(filepath):
     base_name = os.path.basename(filepath)
     name, ext = os.path.splitext(base_name)
     return name, ext
-
-
-def upload_image_path(instance, filename):
-    new_filename = random.randint(1, 999999)
-    name, ext = get_filename_ext(filename)
-    final_filename = '{new_filename}{ext}'.format(new_filename=new_filename, ext=ext)
-    return 'pads/{new_filename}/{final_filename}'.format(
-            new_filename=new_filename,
-            final_filename=final_filename
-            )
 
 class PadQuerySet(models.query.QuerySet):
     def active(self):
