71302805716f7596c7275c5a19c63c06b2b385bd
Jon <jon.drew1492@gmail.com>
2018-10-14 18:06:45 -0400

    Removed add column from migration

--- ribbits/migrations/0010_auto_20181011_1257.py
+++ ribbits/migrations/0010_auto_20181011_1257.py
@@ -13,11 +13,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='ribbit',
-            name='sent_to',
-            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='receiver', to='hoppers.Hopper'),
-        ),
         migrations.AlterField(
             model_name='ribbit',
             name='sent_by',
