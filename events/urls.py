b265c4a1b8953540a3d6858fb31ad9d757bd79e7
Jon <jon.drew1492@gmail.com>
2018-10-15 09:11:38 -0400

    Added update event button

--- events/urls.py
+++ events/urls.py
@@ -4,7 +4,7 @@ from .views import EventCreateView, EventUpdateView, EventListView, EventDetailS
 
 urlpatterns = [
     url(r'^create/$', EventCreateView, name='create'),
-    url(r'^(?P<slug>[\w-]+)/$', EventDetailSlugView.as_view(), name='read'),
     url(r'^update/$', EventUpdateView, name='update'),
+    url(r'^(?P<slug>[\w-]+)/$', EventDetailSlugView.as_view(), name='read'),
     url(r'^$', EventListView.as_view(), name='list'),
 ]
