c3fb95daacf2c5d1b2bdd4aa03501f6b84591e62
Jon <jon.drew1492@gmail.com>
2018-10-17 11:19:45 -0400

    Changed redirect for authenticated users

--- pond/views.py
+++ pond/views.py
@@ -2,4 +2,6 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 def RedirectView(request):
+    if request.user.is_authenticated:
+        return redirect('ribbits:list')
     return redirect('/login/')
